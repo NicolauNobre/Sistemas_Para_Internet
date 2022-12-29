3fd792927e3d83969efe966ae6d1fa9cb08afdd1
Vitor <vitor.arp2010@gmail.com>
2022-12-29 14:14:28 -0300

    Comentários no código

--- controllers/loginController.py
+++ controllers/loginController.py
@@ -1,9 +1,17 @@
+# Descrição: controladora de login, responsável por fazer o login de usuários.
+# Importando o módulo de registro
 from controllers import registerController
 
+# Usando a variável users do módulo de registro
 users = registerController.users
 
+# Função de login
 def login(email, password):
+    # Percorre a lista de usuários
     for user in users:
+        # Verifica se os dados da requisição são iguais aos dados do usuário na lista de usuários
         if user['email'] == email and user['password'] == password:
+            # Caso sejam iguais, retorna uma mensagem de sucesso
             return {'confirm': True, 'message': 'Logged in successfully'}
+    # Caso não sejam iguais, retorna uma mensagem de erro
     return {'confirm': False, 'message': 'Invalid username or password'}
--- controllers/registerController.py
+++ controllers/registerController.py
@@ -1,27 +1,47 @@
+# Descrição: controladora de cadastro, responsável por cadastrar, editar e excluir usuários.
+# Lista de usuários
 users = []
 
+# Funções de cadastro
 def register(username, email, password):
+    # Verifica se o email já existe na lista de usuários
     if (email in [user['email'] for user in users]):
+        # Caso exista, retorna uma mensagem de erro
         return {'confirm': False, 'error': 'Email already exists'}
+    # Caso não exista, adiciona o usuário na lista de usuários
     else:
         users.append({
             'username': username,
             'email': email,
             'password': password
             })
+        # Retorna uma mensagem de sucesso
         return {'confirm': True, 'message': 'Registered successfully'}
 
+# Funções de edição de cadastro
 def update(username, email, password):
+    # Percorre a lista de usuários
     for user in users:
+        # Verifica se o email da requisição é igual ao email do usuário na lista de usuários
         if user['email'] == email:
+            # Caso sejam iguais, atualiza os dados do usuário
             user['username'] = username
             user['password'] = password
+            # Retorna uma mensagem de sucesso
             return {'confirm': True, 'message': 'Updated successfully'}
+    # Caso não sejam iguais, retorna uma mensagem de erro
     return {'confirm': False, 'error': 'User not found'}
 
+# Funções de exclusão de cadastro
 def delete(email, password):
+    # Percorre a lista de usuários
     for user in users:
+        # Verifica se o email e a senha da requisição são iguais 
+        # aos dados do usuário na lista de usuários
         if user['email'] == email and user['password'] == password:
+            # Caso sejam iguais, remove o usuário da lista de usuários
             users.remove(user)
+            # Retorna uma mensagem de sucesso
             return {'confirm': True, 'message': 'Deleted successfully'}
+    # Caso não sejam iguais, retorna uma mensagem de erro
     return {'confirm': False, 'error': 'User not found or password is incorrect'}
